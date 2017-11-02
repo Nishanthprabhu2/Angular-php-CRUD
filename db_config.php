c9b0ad7846224890fbd738214c7290d99df292d9
Prabhu <nishanth.prabhu@epicor.com>
2017-11-02 15:28:27 +0530

    first commit

---
+++ db_config.php
@@ -0,0 +1,9 @@
+<?php
+/*define (DB_USER, "test");
+define (DB_PASSWORD, "test");
+define (DB_DATABASE, "test");
+define (DB_HOST, "localhost");
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);*/
+
+$connect = mysqli_connect("localhost", "test", "test", "test");
+?>
